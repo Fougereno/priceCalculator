73e7b1a2294ad11089ff78062a15ff5b09cd5fc2
Magius1000 <magdaca@yandex.ru>
2022-07-25 18:07:58 +0300

    Added review HTML. See index.php

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<?php 
+<html lang="en"> <!-- Страница на русском! -->
+<?php
     $headtitle = 'New form';
-    require 'head.php'; 
+    require 'head.php';
 ?>
 <body>
     <div class="wrap__100">
+        <!-- Пробелы в (name = "formOrange") - этло так не работает совсем -->
        <form name = "formOrange" class="price js-form main__form" action="send.php"  method="post" >
+        <!-- В price__h1_red модификатор _red используется неверно. его можно применить только к элементу price__h1. А в данном случае у нас оформляется вложенный span.
+        Его можно оформить как еще один элемент блока price или допустимо использовать совсем другой блок, например text-red. Это актуально когда поодбные выделения цветом встречаются еще где-то.
+        Так-же БЭМ допускает миксы. это кгда один HTML-элемент имеет одноверменно классы нескольких блоков. Или является отдельным блоком и элементов другого блока одновременно.  -->
            <h1 class="price__h1">Узнайте самую выгодную стоимость своей квартиры через <span class="price__h1_red">онлайн—калькулятор!</span> </h1>
            <div class="price__wrap">
+                <!-- Не встречал такого. Обычно это либо wrap, дибо row =) -->
                <div class="price__wraprow">
-                   <p class="price__p">Город</p>  
+                   <p class="price__p">Город</p>
+                   <!-- В твоем select нет input-а. А как значение передавать на сервер? писать костыли? ) -->
+                   <!-- Как делаются кастомные селекты: В HTML пишется классический <select> а js его заменяет на кастомный А выбранные значения в кастомном передаются в классический.
+                    Тогда, если у пользователя на работет js - форма все равно будет функцианировать. -->
                    <div class="select">
                         <div class="select__header">
                             <div class="select__current">Москва</div>
+                            <!-- Это не совсем icon этот carret =) или toggler еще местами называют -->
                             <div class="select__icon">
                             <svg class="price__arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <line x1="0.353553" y1="0.646447" x2="7.35355" y2="7.64645" stroke="black"/>
@@ -47,10 +56,12 @@
                </div>
                <div class="price__wraprow">
                    <p class="price__p">Улица</p>
+                   <!-- Тут у тебя input является одновременно элементом 2-х блоков main и price, так еще и одновременно двумя элементами price? =)
+                   Это не ошибка, но не по БЭМ. Тут надо либо модификаторы писатьЮ либо input делать независимым блоком, что кстати было бы правильно. -->
                    <input class="main__input js-input price__input price__submitcheck"  name="street" type="text" >
                </div>
                <div class="price__wraprow">
-                   <p class="price__p">Тип недвижимости</p>  
+                   <p class="price__p">Тип недвижимости</p>
                    <div class="select">
                         <div class="select__header">
                             <div class="select__current">Квартира</div>
@@ -68,9 +79,9 @@
                             <div class="select__item">Прочее</div>
                         </div>
                    </div>
-               </div>     
+               </div>
                <div class="price__wraprow">
-                   <p class="price__p">Тип дома</p>  
+                   <p class="price__p">Тип дома</p>
                    <div class="select">
                         <div class="select__header">
                             <div class="select__current">Панельный</div>
@@ -86,10 +97,12 @@
                             <div class="select__item">Монолитный</div>
                         </div>
                    </div>
-               </div>         
+               </div>
                <div class="price__wraprow">
                    <p class="price__p">Этажность дома</p>
-                   <div class="price__wrapinner">  
+                   <div class="price__wrapinner">
+                        <!-- По БЭМ, если есть модификатор price__number_wrap, значит должен быть и класс элемента price__number в этом же теге аналогично с price__input.
+                        И опять же в одном теге и price__number и price__input -  не по феншую. -->
                         <div class="price__number_wrap  price__input_170w">
                                 <input type="number" class=" price__input" id="rangenumber" min="5" max="120" value="85" oninput="range.value=value">
                         </div>
@@ -101,13 +114,15 @@
                    <div class="price__wrapinner">
                         <div class="price__number_wrap  price__input_170w">
                             <input type="number" class="price__input " id="rangenumber_flat" min="1" max="120" value="85" oninput="range_flat.value=value">
-                        </div>                        
+                        </div>
                         <input type="range" class="price__range" value="85" min="1" max="120" id="range_flat" oninput="rangenumber_flat.value=value"/>
                    </div>
                </div>
                <div class="price__wraprow">
                    <p class="price__p">Количество комнат</p>
                    <div class="price__rooms">
+                        <!-- Тут каждый инпут можно было не оборачивать в отдельные блоки. Рамку с закруглениями дать внешнему блоку обертке с overflow: hidden,
+                        а внутри стилизовать только label, у которых рамка только спрака, кроме последнего, или слева кроме первого - как удобно.  -->
                         <div class="price__roomitem price__roomitem_left">
                             <input class="price__roominput" id="rad1" type="radio" name="radio" value="1">
                             <label class="price__roomlabel" for="rad1">1</label>
@@ -124,7 +139,7 @@
                             <input class="price__roominput" id="rad4" type="radio" name="radio" value="4">
                             <label class="price__roomlabel" for="rad4">4</label>
                         </div>
-                    </div>               
+                    </div>
                </div>
                <div class="price__wraprow">
                    <p class="price__p">Общая площадь</p>
@@ -143,7 +158,7 @@
                    </div>
                </div>
                <div class="price__wraprow">
-                   <p class="price__p">Состояние</p>  
+                   <p class="price__p">Состояние</p>
                    <div class="select">
                         <div class="select__header">
                             <div class="select__current">Не требует ремонта</div>
@@ -159,7 +174,7 @@
                             <div class="select__item">Требует ремонта</div>
                         </div>
                    </div>
-               </div>                     
+               </div>
                <div class="price__wraprow">
                    <p class="price__p">Ваш телефон</p>
                    <input name="phone" type="tel" class="tel price__input price__submitcheck">
@@ -179,14 +194,15 @@
             </div>
         </form>
         <h1 class="price__h1 js-answer"></h1>
-    </div>    
+    </div>
+
     <div class="price__wrap_main">
         <h1 class="price__h1 price__h1_nopad">Выберите программу и рассчитайте условия</h1>
         <div class="wrap__40">
            <form action="#" method = "get" class="price price_gray">
                <div class="price__wrap">
                     <div class="price__wraprow price__wraprow_yellow">
-                        <p class="price__p">Цель кредита</p>  
+                        <p class="price__p">Цель кредита</p>
                         <div class="select">
                                 <div class="select__header">
                                     <div class="select__current">Для семей с детьми</div>
@@ -202,7 +218,7 @@
                                     <div class="select__item">Для бизнеса</div>
                                 </div>
                         </div>
-                    </div>   
+                    </div>
                    <div class="price__wraprow price__wraprow_yellow">
                        <p class="price__p">Стоимость недвижимости</p>
                        <div class="price__wrapcount">
@@ -277,7 +293,7 @@
                    <div class="price__btninner"><a href= "#" class="price__button price__button_yellow">подробнее</a></div>
                    <div class="price__btninner"><a href= "#" class="price__button price__button_yellow" >Оставить заявку </a></div>
                </div>
-        
+
                 </div>
             </form>
         </div>
